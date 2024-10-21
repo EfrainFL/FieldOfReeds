61278afbe8746a753a9c762aaa1bbf44a7490290
EfrainFL <efierrolopez@student.cccs.edu>
2024-10-21 17:52:03 -0600

    Update FieldOfReeds.py

--- FieldOfReeds.py
+++ FieldOfReeds.py
@@ -7,6 +7,9 @@ heroHP = 100
 BGHP = 100
 
 
+playerName = input print("Welcome! Press Enter a name before playing: ") 
+
+print("Narrator 1: You have awoken to discover that you are in a poorly lit room by yourself with vermin. You have no recollection of what happened before this. As you see mounds of sand around you, you believe you are stranded somewhere random in Egypt. The door infront of you looks like a possible exit if you can find a way to unlock it. Find a means of opening the door.")
 
 
 
