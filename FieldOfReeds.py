d1e63e2910c1e5f376950e58c3f3a37f5233aa9e
EfrainFL <efierrolopez@student.cccs.edu>
2024-10-23 11:41:49 -0600

    Update FieldOfReeds.py

--- FieldOfReeds.py
+++ FieldOfReeds.py
@@ -12,6 +12,44 @@ playerName = input print("Welcome! Press Enter a name before playing: ")
 print("You have awoken to discover that you are in a poorly lit room by yourself with vermin. You have no recollection of what happened before this. As you see mounds of sand around you, you believe you are stranded somewhere random in Egypt. The door infront of you looks like a possible exit if you can find a way to unlock it. Find a means of opening the door.")
 
 
+#Enter the Sphinax Room
+
+print("Sphinx: Halt! You must solve my riddles in order to proceed.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("Narrator 2: Great job! You successfully passed my riddles! You may now proceed to the next rooom.")
+
+
+
+print("Narrator 4: After you finish the Sphinx riddles, the Sphinx moves to the side. Depending on what you got correct previously, the greater reward you yield.")
+
+
+print("Narrator 3: You've just entered a room to find an armory of weapons that are at your disposal. You see a Sword, Axe, and Dagger. Although they appear to be old and rusted, they're still usable in combat. These will be useful for any deadly foes that you encounter along the way.")
+
+
+
+
+
+
+
+#Enter the Boss Room
+#Big Bad Evil Guy: 
+print("Welcome to the final room ", playerName " . You have made it very far, but this will be where you will die")
+
+print("Why does the Egypt god Anubis want to fight me!?")
+
+print("Shut Up!!!")
 
 
 
@@ -27,4 +65,7 @@ print("You have awoken to discover that you are in a poorly lit room by yourself
 
 
 
+print("What? How could've I possibly been defeated by a weakling like you? This isn't over, you haven't seen the last of me!")
 
+
+print("Thank you for playing our game! We hope you had as much fun playing it as we did creating the story!")
