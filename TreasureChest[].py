dde8ef8cf555f3a4d38280598a94fdfcad243a65
EfrainFL <efierrolopez@student.cccs.edu>
2024-10-23 14:49:36 -0600

    Update TreasureChest[].py

--- TreasureChest[].py
+++ TreasureChest[].py
@@ -1,23 +1,25 @@
+#start
 
 weapon = [“None”, “Dagger”, “Sword”, “Battle Axe”]
 
 attackup = [0, 1, 2, 3]
 
-Match case:
-	case1 correctAnswers = 0:
+Match correctAnswers :
+	
+	case 1 :
 		print(“Your weapon is ” + weapon[0] + “ and plus damage up is ” + attackup[0])
 		atk += attackup[0]
 
-	case2 correctAnswers = 1:
+	case 2 :
 		print(“Your weapon is ” + weapon[1] + “ and plus damage up is ” + attackup[1])
 		atk += attackup[1]
 
-	case3 correctAnswers = 2:
+	case 3 :
 		print(“Your weapon is ” + weapon[2] + “ and plus damage up is ” + attackup[2])
 		atk += attackup[2]
 
-	case4 correctAnswers = 3:
+	case 4 :
 		print(“Your weapon is ” + weapon[3] + “ and plus damage up is ” + attackup[3])
 		atk += attackup[3]
 
-Return
+#return
