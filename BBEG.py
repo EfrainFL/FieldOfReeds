8200b12aee513c658fd9e1ef704d90823db23283
MisterSean18 <scarter237@student.cccs.edu>
2024-10-28 14:41:08 -0600

    Update BBEG.py

--- BBEG.py
+++ BBEG.py
@@ -1,10 +1,10 @@
-#BBEG()
-#Declarations:
+# BBEG()
+# Declarations:
 TurnCount = 0
 heroAct = "X"
 Exit = False
 
-#Temporary Declarations of Global Variables for Testing Purposes
+# Temporary Declarations of Global Variables for Testing Purposes
 atk = 10
 dmg = 10
 heroHP = 100
@@ -35,23 +35,23 @@ def calcHP():
 	BGHP -= atk
 calcHP()
  
-#Monologue
+# Monologue
 print("Welcome, mortal. I see that you have bested the Sphinx and found your way to my chamber. I am not your enemy, I am simply here to test your resolve one last time. You may have one chance of returning to the land of the living. Best me in combat and I will free your soul, returning you to the life you left behind. Fail, and your soul is mine. Prepare yourself, mortal!")
 
-#User Input
+# User Input
 heroAct = input(" [Fight! Choose A to Attack, B to Block, or D to Dodge.]")
 	
 while Exit == False:
 
 	if BGHP > 0:
-		heroTurn()   #Execute the player choice
+		heroTurn()   # Execute the player choice
 		
-		BGTurn()     #Bad Guy gets a turn. Blocks every 3rd turn
+		BGTurn()     # Bad Guy gets a turn. Blocks every 3rd turn
 		
-		calcHP() 	 #Calculate final hit points for Hero and BadGuy
+		calcHP()     # Calculate final hit points for Hero and BadGuy
 		
 		print(f"You did {atk} damage. You took {dmg} damage. Your remaining HP is {heroHP}.")
-		#endRound
+		# endRound
 		
 		if heroHP > 0:
 			heroAct = input("Choose 'A' to Attack, 'B' to Block, or 'D' to Dodge:" )
