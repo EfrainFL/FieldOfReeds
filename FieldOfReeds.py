e89f27c97d51472764d6eb58de49233f0d2e32e6
EfrainFL <efierrolopez@student.cccs.edu>
2024-11-06 11:25:55 -0700

    Update FieldOfReeds.py

--- FieldOfReeds.py
+++ FieldOfReeds.py
@@ -69,6 +69,32 @@ def treasureChest():
 
 
 
+def main():
+
+	key = input("Press any key to enter")
+	playerName = input print("Welcome! Press enter a name before playing: ") 
+
+	# Narrator 1
+	print("You have awoken to discover that you are in a poorly lit room by yourself with vermin. You have no recollection of what happened before this. As you see mounds of sand around you, you believe you are stranded somewhere random in Egypt. The door infront of you looks like a possible exit if you can find a way to unlock it. Find a means of opening the door.")
+
+
+	# Enter the Sphinx Room
+
+	print("Sphinx: Halt! You must solve my riddles in order to proceed.")
+
+
+
+
+	print("Narrator 4: After you finish the Sphinx riddles, the Sphinx moves to the side. Depending on what you got correct previously, the greater reward you yield.")
+
+	#Enter the treasure chest room
+	print("Narrator 3: You've just entered a room to find an armory of weapons that are at your disposal. You see a Sword, Axe, and Dagger. Although they appear to be old and rusted, they're still usable in combat. These will be useful for any deadly foes that you encounter along the way.")
+
+
+ 	#Exit the treasure chest room
+
+
+   	#Enter the boss room
 
 
 
@@ -77,44 +103,8 @@ def treasureChest():
 
 
 
-key = input("Press any key to enter")
-playerName = input print("Welcome! Press enter a name before playing: ") 
-
-# Narrator 1
-print("You have awoken to discover that you are in a poorly lit room by yourself with vermin. You have no recollection of what happened before this. As you see mounds of sand around you, you believe you are stranded somewhere random in Egypt. The door infront of you looks like a possible exit if you can find a way to unlock it. Find a means of opening the door.")
-
-
-# Enter the Sphinx Room
-
-print("Sphinx: Halt! You must solve my riddles in order to proceed.")
 
 
 
 
-
-
-
-
-
-
-
-
-
-print("Narrator 2: Great job! You successfully passed my riddles! You may now proceed to the next rooom.")
-
-
-print("Narrator 4: After you finish the Sphinx riddles, the Sphinx moves to the side. Depending on what you got correct previously, the greater reward you yield.")
-
-#Enter the treasure chest room
-print("Narrator 3: You've just entered a room to find an armory of weapons that are at your disposal. You see a Sword, Axe, and Dagger. Although they appear to be old and rusted, they're still usable in combat. These will be useful for any deadly foes that you encounter along the way.")
-
-
-
-
-
-# Enter the Boss Room
-# Big Bad Evil Guy Dialogue 
-
-
-
-#End of program
+	#End of the program
