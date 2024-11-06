efbdd3808ebd2d16cfff53211d3992e5d2b053ce
EfrainFL <efierrolopez@student.cccs.edu>
2024-11-06 13:42:48 -0700

    Update FieldOfReeds.py

--- FieldOfReeds.py
+++ FieldOfReeds.py
@@ -1,28 +1,25 @@
-global atk
 atk = 10
 dmg = 10
 heroHP = 100
 BGHP = 100
-global correctAnswer 
 correctAnswers = 0
-global Sword
-global Dagger
-global Axe
-global Nothing
+Sword = "Sword"
+Dagger = "Dagger"
+Axe = "Battle Axe"
+Nothing = "None"
 
 
-#The space where all the methods will be place
-
-key = input("Press any key to enter")
-print("\n")
-playerName = input print("Welcome! Press enter a name before playing: ") 
-print("\n")
-print("You have awoken to discover that you are in a poorly lit room by yourself with vermin. You have no recollection of what happened before this. As you see mounds of sand around you, you believe you are stranded somewhere random in Egypt. The door infront of you looks ancient. You turn the handle of the door and enter through.") 
-print("\n")
-print("As you enter the room, it appears there is a giant Sphinx infront of you. The room begins to shake and you hear a voice.")
-print("\n")
-print("I am the Sphinx, answer these riddles to determine your reward mortal!.")
-print("\n")
+def main_intro():
+	key = input("Press any key to enter")
+	print("\n")
+	playerName = input print("Welcome! Press enter a name before playing: ") 
+	print("\n")
+	print("You have awoken to discover that you are in a poorly lit room by yourself with vermin. You have no recollection of what happened before this. As you see mounds of sand around you, you believe you are stranded somewhere random in Egypt. The door infront of you looks ancient. You turn the handle of the door and enter through.") 
+	print("\n")
+	print("As you enter the room, it appears there is a giant Sphinx infront of you. The room begins to shake and you hear a voice.")
+	print("\n")
+	print("I am the Sphinx, answer these riddles to determine your reward mortal!.")
+	print("\n")
 
 #Sphinx mehtod
 
@@ -60,39 +57,34 @@ def treasureChest():
 	attackup = [0, 1, 2, 3]
 
 	i = 0
-
-	while correctAnswers < 4 :
-	
-		if correctAnswers == 1 :
+	if correctAnswers == 1 :
 			print(“Your weapon is ” + weapon[i] + “ and plus damage up is ” + attackup[i])
 			atk += attackup[i]
 
-		elif correctAnswers == 2 :
+	elif correctAnswers == 2 :
 			print(“Your weapon is ” + weapon[i+1] + “ and plus damage up is ” + attackup[i+1])
 			atk += attackup[i+1]
 
-		elif correctAnswers == 3 :
+	elif correctAnswers == 3 :
 			print(“Your weapon is ” + weapon[i+2] + “ and plus damage up is ” + attackup[2])
 			atk += attackup[i+2]
 
-		elif correctAnswers == 4 :
+	elif correctAnswers == 4 :
 			print(“Your weapon is ” + weapon[i+3] + “ and plus damage up is ” + attackup[i+3])
 			atk += attackup[i+3]
-		else:
+	else:
 			print("Somthing went wrong")
 
 
-print("After you finish the Sphinx riddles, the Sphinx moves to the side. Depending on what you got correct previously, the greater reward you yield.")
+	print("After you finish the Sphinx riddles, the Sphinx moves to the side. Depending on what you got correct previously, the greater reward you yield.")
 
 	# Enter the treasure chest room
-print("You've just entered a room to find an armory of weapons that are at your disposal. You see a Sword, Axe, and Dagger. Although they appear to be old and rusted, they're still usable in combat. These will be useful for any deadly foes that you encounter along the way.")
+	print("You've just entered a room to find an armory of weapons that are at your disposal. You see a Sword, Axe, and Dagger. Although they appear to be old and rusted, they're still usable in combat. These will be useful for any deadly foes that you encounter along the way.")
 
+def main():
+    game_intro()   
+    sphinx()       
+    treasureChest()  
 
- 	#Exit the treasure chest room
-
-
-   	#Enter the boss room
-
-
-
-	#End of the program
+if __name__ == '__main__':
+    main()
