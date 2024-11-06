d120f8b78de9ef6650a6b647ab4f9035bef454e6
EfrainFL <efierrolopez@student.cccs.edu>
2024-11-06 11:52:55 -0700

    Update FieldOfReeds.py

--- FieldOfReeds.py
+++ FieldOfReeds.py
@@ -13,31 +13,31 @@ correctAnswers = 0
 
 def sphinx():
 	
-  	print(" I am the Sphinx, answer these riddles to determine your reward mortal!. ”)
+  	print("I am the Sphinx, answer these riddles to determine your reward mortal!.")
 
 # Declarations
-	riddleAnswer1 = Lettuce
-	riddleAnswer2 = Rain
-	riddleAnswer3 = Darkness 
-	userAnswer1 = input("What has a head, but no brain?”)
-	userAnswer2 = input("What comes down, but never goes up?”)
-	userAnswer3 = input("The more of this there is, the less you see… What is it?”)
+	riddleAnswer1 = "Lettuce"
+	riddleAnswer2 = "Rain"
+	riddleAnswer3 = "Darkness"
+	userAnswer1 = input("What has a head, but no brain?")
+	userAnswer2 = input("What comes down, but never goes up?")
+	userAnswer3 = input("The more of this there is, the less you see… What is it?")
 
 
 	if userAnswer1 == riddleAnswer1
-		print("That is correct. Onto the next riddle.”)
+		print("That is correct. Onto the next riddle.")
 		correctAnswers += 1
 	else: 
-		print("That is incorrect. Onto the next riddle.”)
+		print("That is incorrect. Onto the next riddle.")
 
 	if userAnswer2 == riddleAnswer2
-		print("That is correct. Onto the next riddle.”)
+		print("That is correct. Onto the next riddle.")
 		correctAnswers += 1
 	else: 
 		print("That is incorrect. Onto the next riddle.”)
 
 	if userAnswer3 == riddleAnswer3
-		print("That is correct. You have heard all my riddles, claim your reward.”)
+		print("That is correct. You have heard all my riddles, claim your reward.")
 		correctAnswers += 1
 
 
