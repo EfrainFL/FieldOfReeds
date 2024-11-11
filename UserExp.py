3396396d5dbae84de42f81305bad0dbe15916519
MisterSean18 <scarter237@student.cccs.edu>
2024-11-11 13:14:07 -0700

    Update UserExp.py

--- UserExp.py
+++ UserExp.py
@@ -4,15 +4,15 @@ Start
 
 print("Welcome! Press any key to begin playing.") 
 
-print("Narrator 1: You have awoken to discover that you are in a poorly lit room by yourself with vermin. You have no recollection of what happened before this. As you see mounds of sand around you, you believe you are stranded somewhere random in Egypt. The door infront of you looks like a possible exit if you can find a way to unlock it. Find a means of opening the door.")
+print("Narrator: You have awoken to discover that you are in a poorly lit room by yourself with vermin. You have no recollection of what happened before this. As you see mounds of sand around you, you believe you are stranded somewhere random in Egypt. The door infront of you looks like a possible exit if you can find a way to unlock it. Find a means of opening the door.")
 
 print("Sphinx: Halt! You must solve one of my riddles to proceed.")
 
-print("Narrator 2: Great job! You successfully passed the riddle! You may now proceed.")
+print("Narrator: Great job! You successfully passed the riddle! You may now proceed.")
 
-print("Narrator 3: You've just entered a room to find an armory of weapons that are at your disposal. You see a Sword, Axe, and Dagger. Although they appear to be old and rusted, they're still usable in combat. These will be useful for any deadly foes that you encounter along the way.")
+print("Narrator: You've just entered a room to find an armory of weapons that are at your disposal. You see a Sword, Axe, and Dagger. Although they appear to be old and rusted, they're still usable in combat. These will be useful for any deadly foes that you encounter along the way.")
 
-print("Narrator 4: After you finish the Sphinx riddles, the Sphinx moves to the side. Depending on what you got correct previously, the greater reward you yield.")
+print("Narrator: After you finish the Sphinx riddles, the Sphinx moves to the side. Depending on what you got correct previously, the greater reward you yield.")
 
 print("Big Bad Evil Guy: What? How could've I possibly been defeated by a weakling like you? This isn't over, you haven't seen the last of me!")
 
