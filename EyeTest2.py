1c6633644513eee829da0467d8eccd31856bf157
EfrainFL <efierrolopez@student.cccs.edu>
2024-11-22 15:55:04 -0700

    Create EyeTest2.py

---
+++ EyeTest2.py
@@ -0,0 +1,30 @@
+#Attempt at getting ASCII Image to display as splash screen at the beginning o the game
+#Difficulty getting each line to print at the same width, doesn't display properly in terminal
+#Tried using {:80} statements to control width, didn't work
+#Tried simple print(f"") statements, didn't work
+#Deleted prior attempts to keep it clean
+#Feel Free to alter this
+
+print("                              ..,,,,,,,,,,,,,,,.                         ")
+print("                      ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,.               ")
+print("              ,,,,,,,,,,,,,,,,,,.                ,,,,,,,,,,,,,,,,,       ")
+print("  .,,,,,,,,,,,,,,,,,,,,             ,,,,,,,,,              .,,,,,,,,,,,,,,,,,,,,")
+print("  ,,,,,,,,,,,,,.        .,,,,,,,,,,,,,,,,,,,,,,,,,,,,,.            .,,,,,,,,,,,,")
+print("  ,,,.         .,,,,,,,,,,,,,,. ,,,,,,,,,,,,,,   .,,,,,,,,,,,,                  ")
+print("      .,,,,,,,,,,,,.            ,,,,,,,,,,,,,,,            ,,,,,,,,,,,,,,,,,,,,,")
+print(" .,,,,,,,,,,.                   ,,,,,,,,,,,,,,                  ,,,,,,,,,,,,,,,,")
+print(" .,,,,,,,,,,,,.                  .,,,,,,,,,,,                  .,,,,,,,,,.      ")
+print("        .,,,,,,,,,,,,.                ...                  ,,,,,,,,,            ")
+print("                .,,,,,,,,,,,,,,,,,,,,         ,,,,,,,,,,,,,,,,,,,               ")
+print("                        .,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,                ")
+print("                                               .,,,,,,,,   ,,,,                 ")
+print("     .,,,,.                                  .,,,,,,,,     ,,,,                 ")
+print("  ,,.   ,,,,,.                             ,,,,,,,,.      ,,,,,,,               ")
+print(" ,.     ,,,,,.                          ,,,,,,,,,        .,,,,,,,,              ")
+print(",,                                   ,,,,,,,,,           ,,,,,,                 ")
+print(" ,                                ,,,,,,,,,.            .,,,,                   ")
+print(" ,,                            ,,,,,,,,,                .,,.                    ")
+print("  .,,                      ,,,,,,,,,,                   .,.                     ")
+print("    .,,,             .,,,,,,,,,,,                        ,                      ")
+print("        ,,,,,,,,,,,,,,,,,,,.                             ,                      ")
+print("              .. .                                                              ")
