91f68d85868f350c589b3e543a51e41697e222a2
Nonesuch <jamesrglass@live.com>
2024-11-22 14:09:39 -0700

    Update EyeTest.py

--- EyeTest.py
+++ EyeTest.py
@@ -1,5 +1,8 @@
 #Attempt at getting ASCII Image to display as splash screen at the beginning o the game
 #Difficulty getting each line to print at the same width, doesn't display properly in terminal
+#Tried using {:80} statements to control width, didn't work
+#Tried simple print(f"") statements, didn't work
+#Feel Free to alter this
 
 print("                              ..,,,,,,,,,,,,,,,.                         ",
 "                      ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,.                      ",
